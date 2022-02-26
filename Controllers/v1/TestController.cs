a56617fd787de95f77ec476fec158d6083cd6ed8
Saleh <s.ghezelbash10@gmail.com>
2022-02-27 00:28:51 +0330

    version first commit

---
+++ Controllers/v1/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_books.Controllers.v1
+{
+    [ApiVersion("1.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=1.0
+    [ApiVersion("1.1")]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")] // Url versioning for example https://localhost:44359/api/v1/test/get-test-data
+    public class TestController:ControllerBase
+    {
+        [HttpGet("get-test-data")]
+        public IActionResult Get()
+        {
+            return Ok("This is a Test controller V1");
+        }
+
+        [HttpGet("get-test-data")]
+        [MapToApiVersion("1.1")]
+        public IActionResult Getv11()
+        {
+            return Ok("This is a Test controller V1.1");
+        }
+    }
+}
---
+++ Controllers/v2/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace my_books.Controllers.v2
+{
+    [ApiVersion("2.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=2.0
+    [ApiController]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]  // Url versioning for example https://localhost:44359/api/v2/test/get-test-data
+    public class TestController:ControllerBase
+    {
+        [HttpGet("get-test-data")]
+        public IActionResult Get()
+        {
+            return Ok("This is a Test controller V2");
+        }
+    }
+}
