3650684b901dec12c18c8ac07a42831657415bd5
Saleh <s.ghezelbash10@gmail.com>
2022-02-27 22:11:39 +0330

    correct conflict after merge by logging

--- Controllers/v1/TestController.cs
+++ Controllers/v1/TestController.cs
@@ -2,19 +2,11 @@
 
 namespace my_books.Controllers.v1
 {
-<<<<<<< HEAD
-    [ApiVersion("1.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=1.0
-    [ApiVersion("1.1")]
-    [ApiController]
-    [Route("api/[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")] // Url versioning for example https://localhost:44359/api/v1/test/get-test-data
-=======
     //[ApiVersion("1.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=1.0
     //[ApiVersion("1.1")]
     [ApiController]
     [Route("api/[controller]")]
     //[Route("api/v{version:apiVersion}/[controller]")] // Url versioning for example https://localhost:44359/api/v1/test/get-test-data
->>>>>>> logging
     public class TestController:ControllerBase
     {
         [HttpGet("get-test-data")]
@@ -23,20 +15,11 @@ namespace my_books.Controllers.v1
             return Ok("This is a Test controller V1");
         }
 
-<<<<<<< HEAD
-        [HttpGet("get-test-data")]
-        [MapToApiVersion("1.1")]
-        public IActionResult Getv11()
-        {
-            return Ok("This is a Test controller V1.1");
-        }
-=======
         //[HttpGet("get-test-data")]
         //[MapToApiVersion("1.1")]
         //public IActionResult Getv11()
         //{
         //    return Ok("This is a Test controller V1.1");
         //}
->>>>>>> logging
     }
 }
--- Controllers/v2/TestController.cs
+++ Controllers/v2/TestController.cs
@@ -2,19 +2,6 @@
 
 namespace my_books.Controllers.v2
 {
-<<<<<<< HEAD
-    [ApiVersion("2.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=2.0
-    [ApiController]
-    [Route("api/[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")]  // Url versioning for example https://localhost:44359/api/v2/test/get-test-data
-    public class TestController:ControllerBase
-    {
-        [HttpGet("get-test-data")]
-        public IActionResult Get()
-        {
-            return Ok("This is a Test controller V2");
-        }
-=======
     //[ApiVersion("2.0")] // Query versioning for example https://localhost:44359/api/test/get-test-data?api-version=2.0
     //[ApiController]
     [Route("api/[controller]")]
@@ -26,6 +13,5 @@ namespace my_books.Controllers.v2
         //{
         //    return Ok("This is a Test controller V2");
         //}
->>>>>>> logging
     }
 }
